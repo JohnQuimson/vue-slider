8cbcf5fc391daa005fc14b115f97e62a0fc06acb
JohnQuimson <john.quimson777@gmail.com>
2023-11-27 16:47:51 +0100

    feat: current img

--- js/script.js
+++ js/script.js
@@ -1,12 +1,26 @@
 'use strict';
 
-const { createApp, ref } = Vue;
+const { createApp } = Vue;
 
 createApp({
-  setup() {
-    const message = ref('Hello vue!');
+  data() {
     return {
-      message,
+      titoloPrincipale: 'Vue Slider',
+      film: {
+        foto: [
+          'img/01.jpg',
+          'img/02.jpg',
+          'img/03.jpg',
+          'img/04.jpg',
+          'img/05.jpg',
+        ],
+      },
+      currentImage: 0,
+      info: {
+        titolo: 'TESLA',
+        description: `Tesla è un'azienda statunitense specializzata nella produzione di auto elettriche`,
+      },
     };
   },
+  methods: {},
 }).mount('#app');
